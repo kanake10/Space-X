287c55555ae3027bcd35623650b3e00c0a897430
kanake10 <10kanake@gmail.com>
2022-04-28 23:52:47 +0300

    navigation

--- app/src/main/java/com/example/spacex/data/repository/SpaceRepositoryImpl.kt
+++ app/src/main/java/com/example/spacex/data/repository/SpaceRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.spacex.data.repository
 
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.coroutines.await
+import com.example.spacex.LaunchDetailsQuery
 import com.example.spacex.LaunchesQuery
 import com.example.spacex.data.api.SpaceXApi
 import javax.inject.Inject
@@ -11,5 +12,10 @@ class SpaceRepositoryImpl @Inject constructor(
 ) : SpaceRepository{
     override suspend fun queryLaunches(): Response<LaunchesQuery.Data> {
         return webService.getApolloClient().query(LaunchesQuery()).await()
+    }
+
+    override suspend fun queryLaunchDetails(id: String): Response<LaunchDetailsQuery.Data> {
+        return webService.getApolloClient().query(LaunchDetailsQuery(id)).await()
+
     }
 }
