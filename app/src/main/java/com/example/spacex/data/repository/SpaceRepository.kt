6c4cb6b5877a20f7a29b68cac9fc3cc8e016461d
kanake10 <10kanake@gmail.com>
2022-04-28 23:55:11 +0300

    repository

--- app/src/main/java/com/example/spacex/data/repository/SpaceRepository.kt
+++ app/src/main/java/com/example/spacex/data/repository/SpaceRepository.kt
@@ -1,8 +1,11 @@
 package com.example.spacex.data.repository
 
 import com.apollographql.apollo.api.Response
+import com.example.spacex.LaunchDetailsQuery
 import com.example.spacex.LaunchesQuery
 
 interface SpaceRepository {
     suspend fun queryLaunches(): Response<LaunchesQuery.Data>
+
+    suspend fun queryLaunchDetails(id: String) : Response<LaunchDetailsQuery.Data>
 }
